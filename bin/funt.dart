5fc5243806665f53deb7d3d97aa2496a32cf189c
pacificbhoy <isaacjuniorndukwe@gmail.com>
2024-03-06 17:46:00 +0100

    first commit

---
+++ bin/funt.dart
@@ -0,0 +1,39 @@
+
+
+void main(List<String> arguments) {
+  //function called addTwo that takes two numbers as arguments and returns the sum of those two numbers
+  int addTwo(int a, int b) {
+  return a + b;
+}
+print(addTwo(5, 8));
+
+//function called subtractTwo that takes two numbers as arguments and returns the difference of those two numbers
+  int subtractTwo(int a, int b) {
+  return a - b;
+}
+print(subtractTwo(9, 13));
+
+//function called multiplyTwo that takes two numbers as arguments and returns the product of those two numbers
+double multiplyTwo(double a, double b) {
+  return a * b;
+}
+print(multiplyTwo(12.4, 1.5));
+
+//function called divideTwo that takes two numbers as arguments and returns the quotient of those two numbers
+num divideTwo(num a, num b) {
+  return a / b;
+}
+print(divideTwo(15, 3));
+
+//function called stringLength that takes an argument of type String and returns the length of that string
+stringLength() {
+  String name = "hippopotamus";
+  return name.length;
+}
+print(stringLength());
+
+//function called getFirstElement that takes a list as an argument and returns the first element of that list
+List getFirstElement = ["ferrari", "bugatti", "benz", "toyota"];
+print(getFirstElement[0]);
+
+}
---
+++ test/funt_test.dart
@@ -0,0 +1,8 @@
+import 'package:funt/funt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
